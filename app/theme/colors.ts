150919e261b3cbb7f3a8dee5679332d8bf408945
Clara <clararemotto@gmail.com>
2025-08-16 16:05:06 -0300

    style: add theme folder for centralized styling

---
+++ app/theme/colors.ts
@@ -0,0 +1,71 @@
+export type Colors = {
+  background: string;
+  backgroundSecondary: string;
+  white: string;
+  black: string;
+  border: string;
+  gray400: string;
+  gray800: string;
+  blue200: string;
+  blue300: string;
+  blue500: string;
+  purple400: string;
+  purple500: string;
+  redLight: string;
+  redNormal: string;
+  greenLigth: string;
+  greenNormal: string;
+  yellowLight: string;
+  yellowNormal: string;
+  textColor: string;
+  textColorBlue: string;
+  textColorIcon: string;
+};
+
+export const lightColors = {
+  background: "#f7f7f7",
+  backgroundSecondary: "#E0E0E0",
+  white: "#FFFFFF",
+  black: "#000000",
+  border: "#BDBDBD",
+  gray400: "#BDBDBD",
+  gray800: "#424242",
+  blue200: "#B0C0D0",
+  blue300: "#003366",
+  blue500: "#002952",
+  purple400: "#7E57C2",
+  purple500: "#673AB7",
+  redLigth: "#FF3232",
+  redNormal: "#E20303",
+  greenLigth: "#247901",
+  greenNormal: "#164D00",
+  yellowLigth: "#FFB041",
+  yellowNormal: "#FF9500",
+  textColor: "#000000",
+  textColorBlue: "#002952",
+  textColorIcon: "#424242",
+};
+
+export const darkColors = {
+  background: "#222222",
+  backgroundSecondary: "#333333",
+  white: "#FFFFFF",
+  black: "#000000",
+  border: "#444444",
+  gray400: "#BDBDBD",
+  gray800: "#424242",
+  blue200: "#B0C0D0",
+  blue300: "#003366",
+  blue500: "#002952",
+  purple400: "#7E57C2",
+  purple500: "#673AB7",
+  redLigth: "#FF3232",
+  redNormal: "#E20303",
+  greenLigth: "#3dcc04ff",
+  greenNormal: "#31a503ff",
+  yellowLigth: "#FFB041",
+  yellowNormal: "#FF9500",
+  textColor: "#f7f7f7",
+  textColorBlue: "#B0C0D0",
+  textColorIcon: "#FFFFFF",
+};
