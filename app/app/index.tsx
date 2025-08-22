d438fb979b5f0640fabf674838e98085d136afb1
Clara <clararemotto@gmail.com>
2025-08-22 11:22:39 -0300

    feat: update login view and add FontAwesome

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,11 +1,12 @@
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { InitialView } from "../pages/initial/initial.view";
+import { LoginView } from "../pages/login/login.view";
 
 export default function Index() {
     return(
         <SafeAreaProvider>
             <SafeAreaView>
-                <InitialView/>
+                <LoginView/>
             </SafeAreaView>
         </SafeAreaProvider>
     )
