4bd638b93d263c0dbb7d072c83370b4cb23a361d
Clara <clararemotto@gmail.com>
2025-08-23 14:39:55 -0300

    feat: styling theme button as needed

--- app/components/ThemeButton/index.tsx
+++ app/components/ThemeButton/index.tsx
@@ -1,12 +1,31 @@
-import { TouchableOpacity } from "react-native"
-import { ThemeButtonProps } from "./themeButton.types"
-import { useTheme } from "@react-navigation/native"
+import { darkColors, lightColors } from "@/app/theme/colors";
+import { useTheme } from "@/app/theme/ThemeContext";
 import { Moon, Sun } from "lucide-react-native";
+import { TouchableOpacity } from "react-native";
+import { themeButtonStyles } from "./themeButton.styles";
+import { ThemeButtonProps } from "./themeButton.types";
 
-export const ThemeButton = ({ isDark, toggleTheme} : ThemeButtonProps) => {
-    return(
-        <TouchableOpacity onPress={toggleTheme}>
-            {isDark ? <Moon/> : <Sun/>}
-        </TouchableOpacity>
-    )
-}
+export const ThemeButton = ({ isDark, toggleTheme }: ThemeButtonProps) => {
+  const { colors } = useTheme();
+  const styles = themeButtonStyles(colors);
+
+  return (
+    <TouchableOpacity
+      onPress={toggleTheme}
+      style={[
+        styles.button,
+        {
+          backgroundColor: isDark
+            ? darkColors.backgroundSecondary
+            : lightColors.backgroundSecondary,
+        },
+      ]}
+    >
+      {isDark ? (
+        <Moon color={colors.textColor} />
+      ) : (
+        <Sun color={colors.textColor} />
+      )}
+    </TouchableOpacity>
+  );
+};
