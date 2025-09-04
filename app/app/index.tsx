57953ccce7d12e08821edb9a66b9d2b1a80955aa
Clara <clararemotto@gmail.com>
2025-09-04 09:13:15 -0300

    fix: fix index file

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,14 +1,13 @@
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { ThemeProvider } from "../theme/ThemeContext";
 import { InitialView } from "../pages/initial/initial.view";
-import { LoginView } from "../pages/login/login.view";
+import { ThemeProvider } from "../theme/ThemeContext";
 
 export default function Index() {
     return(
         <ThemeProvider>
             <SafeAreaProvider>
             <SafeAreaView>
-                <LoginView/>
+                <InitialView/>
             </SafeAreaView>
         </SafeAreaProvider>
         </ThemeProvider>
