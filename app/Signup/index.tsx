7cbf60b1c19bf90965bffe251042672d3862a417
Clara <clararemotto@gmail.com>
2025-09-18 10:36:19 -0300

    feat: creating routes to the sitemap

---
+++ app/Signup/index.tsx
@@ -0,0 +1,15 @@
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { SignupView } from "../pages/Signup/signup.view";
+import { ThemeProvider } from "../theme/ThemeContext";
+
+export default function LoginPage() {
+  return (
+    <ThemeProvider>
+      <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1 }}>
+          <SignupView />
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </ThemeProvider>
+  );
+}
